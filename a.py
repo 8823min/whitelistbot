65732da722f21a6d3343f0a2d5245a98048c4f5b
8823min <129752233+8823min@users.noreply.github.com>
2024-02-07 15:09:42 +0900

    Update a.py

--- a.py
+++ a.py
@@ -8,10 +8,8 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
-# whitelist.jsonファイルのパスをセットします
 WHITELIST_PATH = 'YOUR JSON FILE PATH'
 
-# Mojang APIのベースURLをセットします
 MOJANG_API_BASE_URL = 'https://api.mojang.com/users/profiles/minecraft/'
 
 def is_already_whitelisted(mcid):
@@ -26,11 +24,9 @@ def is_already_whitelisted(mcid):
 async def on_ready():
     print('ログインしました')
 
-    # アクティビティを設定
     new_activity = f"ある8鯖"
     await client.change_presence(activity=discord.Game(new_activity))
 
-    # スラッシュコマンドを同期
     await tree.sync()
 
 async def fetch_mojang_api(session, url):
@@ -39,7 +35,6 @@ async def fetch_mojang_api(session, url):
 
 @tree.command(name='list', description='ホワイトリストを登録します。')
 async def add_to_whitelist(interaction: discord.Interaction, mcid: str):
-    # すでにホワイトリストに登録されているかをチェックします
     if is_already_whitelisted(mcid):
         embed = discord.Embed(
             description=f"⚠️ **{mcid}** さんはすでにホワイトリストに登録されています！",
@@ -48,26 +43,21 @@ async def add_to_whitelist(interaction: discord.Interaction, mcid: str):
         await interaction.response.send_message(embed=embed)
         return
 
-    # Mojang APIからUUIDを取得します
     mojang_api_url = MOJANG_API_BASE_URL + mcid
     async with aiohttp.ClientSession() as session:
         try:
-            # タイムアウトを設定してAPIからの応答を待ちます
             response_data = await asyncio.wait_for(fetch_mojang_api(session, mojang_api_url), timeout=2)
             uuid = response_data.get('id')
-            # whitelist.jsonファイルを読み込み、新しいエントリを追加します
             with open(WHITELIST_PATH, 'r+') as f:
                 whitelist = json.load(f)
                 whitelist.append({
                     'uuid': uuid,
                     'name': mcid
                 })
-                # ファイルを書き込みモードで開き、新しいエントリを書き込みます
                 f.seek(0)
                 json.dump(whitelist, f, indent=2)
                 f.truncate()
 
-            # Embedを作成して、指定されたMCIDの追加を通知します
             embed = discord.Embed(
                 description=f"✅ **{mcid}** さんをホワイトリストに追加しました。",
                 color=0x00ff00
